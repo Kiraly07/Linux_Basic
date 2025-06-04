feafe2f9c02ed9a110847d30f5cb36759037e547
TuanAnh <153625413+Kiraly07@users.noreply.github.com>
2025-06-04 09:31:46 +0700

    Update remove_chall_users.sh

--- remove_chall_users.sh
+++ remove_chall_users.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-users=(chall1 chall2 chall3 chall4 chall5 chall6 chall7 chall8 chall9 chall10)
+users=(chall1 chall2 chall3 chall4 chall5 chall6 chall7 chall8 )
 
 for user in "${users[@]}"; do
     sudo userdel -r -f "$user" 2>/dev/null
 done
 
-# Xóa các user khỏi AllowUsers trong sshd_config
 if grep -q "^AllowUsers" /etc/ssh/sshd_config; then
-    sudo sed -i 's/\(AllowUsers.*\)\(chall1\|chall2\|chall3\|chall4\|chall5\|chall6\|chall7\|chall8\|chall9\|chall10\)\s*//g' /etc/ssh/sshd_config
+    sudo sed -i 's/\(AllowUsers.*\)\(chall1\|chall2\|chall3\|chall4\|chall5\|chall6\|chall7\|chall8\)\s*//g' /etc/ssh/sshd_config
 fi
 
-# Khởi động lại dịch vụ SSH để áp dụng thay đổi
 sudo systemctl restart ssh || sudo service ssh restart
 
 echo "[+] Đã xóa toàn bộ user challenge và cập nhật sshd_config."
